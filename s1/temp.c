a7a8767781ac43d0363d5f9d43fb2930ca9e4e29
errantspark <errantspark@gmail.com>
2020-05-19 21:33:27 -0700

    page 12

--- s1/temp.c
+++ s1/temp.c
@@ -3,15 +3,15 @@
 /* print Fahrenheit-Celsius table */
 main()
 {
-  int fahr, cels; 
+  float fahr, cels; 
   int lower = 0;
   int upper = 300;
   int step  = 20;
 
   fahr = lower;
   while (fahr <= upper) {
-    cels = 5 * (fahr-32) /9;
-    printf("%d\t%d\n", fahr, cels);
+    cels = (5.0/9) * (fahr-32);
+    printf("%6.0f\t%5.1f\n", fahr, cels);
     fahr += step;
   }
 }
